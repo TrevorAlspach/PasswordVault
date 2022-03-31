2590de026d32499178e858ecbbe0645d4aaab197
Andr3wx <apf19e@my.fsu.edu>
2022-03-31 18:42:34 -0400

    Made some changes to login screen
    Added a submit button for when user is ready to attempt to login, changed the input label to hide the characters with stars. If password matches "password" (Will change later to users password), changes screens to screen after login. If password is wrong, started a counter that will reset passwords after 10 attempts.

--- gui/windows/login_window.py
+++ gui/windows/login_window.py
@@ -1,5 +1,6 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QMainWindow, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLineEdit, QMainWindow, QLabel, QPushButton
 from PySide6 import QtCore
+from gui.windows.main_window import MainWindow
 
 
 class LoginWindow(QMainWindow):
@@ -9,14 +10,36 @@ class LoginWindow(QMainWindow):
         self.setWindowTitle("Password Vault")
         self.setFixedSize(540, 720)
         self.setCentralWidget(self.display_login())
+        self.userInput = ""
+        self.w = None
+        self.counter = 0
 
     def display_login(self):
         widget = QWidget()
         layout = QVBoxLayout()
         password_input = QLineEdit()
+        password_input.setEchoMode(QLineEdit.Password)  # Hides password input
         layout.setAlignment(QtCore.Qt.AlignCenter)
         layout.addWidget(QLabel("Enter Master Password"))
         layout.addWidget(password_input)
+        password_input.textChanged.connect(self.passwordChange)
+        loginButton = QPushButton("Submit")
+        loginButton.clicked.connect(self.checkCredentials)
+        layout.addWidget(loginButton)
         widget.setLayout(layout)
 
         return widget
+
+    def checkCredentials(self):
+        if self.userInput == "password":
+            self.w = MainWindow()
+            self.w.show()
+            self.close()
+        else:
+            self.counter = self.counter + 1
+            layout = self.centralWidget().layout()
+            layout.addWidget(QLabel(str(10-self.counter) + " password attempts until data is cleared."))
+
+    def passwordChange(self, text):
+        self.userInput = text
+
--- gui/windows/main_window.py
+++ gui/windows/main_window.py
@@ -5,5 +5,6 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Password Vault")
+        self.setFixedSize(540, 720)
         self.centralWidget = QWidget()
         self.setCentralWidget(self.centralWidget)
