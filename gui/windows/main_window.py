d20f315507a42b99be167c5b21666d85f3b3515d
Andr3wx <apf19e@my.fsu.edu>
2022-04-08 15:20:18 -0400

    Displays account information in table view within main window
    Created a table that holds site name, username, and password

--- gui/windows/main_window.py
+++ gui/windows/main_window.py
@@ -1,7 +1,52 @@
 from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QVBoxLayout
-from PySide6 import QtCore
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtCore import Qt
 import sqlite3 as sql
 from gui.windows.add_password import AddPasswordWindow
+
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        Qt.AlignCenter
+        self.horizontalHeaders = [''] * 3
+        self.setHeaderData(0, Qt.Horizontal, "Site")
+        self.setHeaderData(1, Qt.Horizontal, "Username")
+        self.setHeaderData(2, Qt.Horizontal, "Password")
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            # See below for the nested-list data structure.
+            # .row() indexes into the outer list,
+            # .column() indexes into the sub-list
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        return len(self._data[0])
+
+    def setHeaderData(self, section, orientation, data, role=Qt.EditRole):
+        if orientation == Qt.Horizontal and role in (Qt.DisplayRole, Qt.EditRole):
+            try:
+                self.horizontalHeaders[section] = data
+                return True
+            except:
+                return False
+        return super().setHeaderData(section, orientation, data, role)
+
+    def headerData(self, section, orientation, role=Qt.DisplayRole):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            try:
+                return self.horizontalHeaders[section]
+            except:
+                pass
+        return super().headerData(section, orientation, role)
 
 
 class MainWindow(QMainWindow):
@@ -24,12 +69,17 @@ class MainWindow(QMainWindow):
         refreshButton.clicked.connect(self.refresh)
         layout.addWidget(refreshButton)
 
-        widget.setLayout(layout)
+        # widget.setLayout(layout)
         with sql.connect('db/database.db') as db:
             cur = db.cursor()
             cur.execute("SELECT * FROM Passwords")
-            print(cur.fetchall())
-
+            data = cur.fetchall()
+            print(data)
+        table = QtWidgets.QTableView()
+        model = TableModel(data)
+        table.setModel(model)
+        layout.addWidget(table)
+        widget.setLayout(layout)
         return widget
 
     def addPassword(self):
@@ -40,5 +90,3 @@ class MainWindow(QMainWindow):
         self.w = MainWindow()
         self.w.show()
         self.close()
-
-
